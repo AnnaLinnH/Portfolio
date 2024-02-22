96b527a73a7464197576b80127eaeb4d3ee52b8e
AnnaLinnH <126760947+AnnaLinnH@users.noreply.github.com>
2024-02-22 17:44:45 +0100

    Content creation færdig

---
+++ script.js
@@ -0,0 +1,115 @@
+/*const videos = document.querySelectorAll(".contentVideo");
+
+videos.forEach(video => {
+  let isHovered = false;
+
+  video.addEventListener("mouseover", function() {
+    isHovered = true;
+    if (video.paused) {
+      video.play();
+    }
+  });
+
+  video.addEventListener("mouseout", function() {
+    isHovered = false;
+    if (!isHovered && !video.paused) {
+      video.pause();
+    }
+  });
+});*/
+
+/*video1*/
+function playVideo() {
+    let video = document.getElementById("tiktok1");
+    video.play();
+}
+
+function pauseVideo() {
+    let video = document.getElementById("tiktok1");
+    video.pause();
+    //video.currentTime = 0;
+}
+
+/*video2*/
+
+function playVideoto() {
+  var videoto = document.getElementById("tiktok2");
+  videoto.play();
+}
+
+function pauseVideoto() {
+  var videoto = document.getElementById("tiktok2");
+  videoto.pause();
+}
+
+/*video3*/
+
+function playVideotre() {
+  var videotre = document.getElementById("tiktok3");
+  videotre.play();
+}
+
+function pauseVideotre() {
+  var videotre = document.getElementById("tiktok3");
+  videotre.pause();
+}
+
+/*video4*/
+
+function playVideofire() {
+  var videofire = document.getElementById("tiktok4");
+  videofire.play();
+}
+
+function pauseVideofire() {
+  var videofire = document.getElementById("tiktok4");
+  videofire.pause();
+}
+
+/*video5*/
+
+function playVideofem() {
+    var videofem = document.getElementById("tiktok5");
+    videofem.play();
+  }
+  
+  function pauseVideofem() {
+    var videofem = document.getElementById("tiktok5");
+    videofem.pause();
+  }
+
+/*video6*/
+
+function playVideoseks() {
+    var videoseks = document.getElementById("tiktok6");
+    videoseks.play();
+  }
+  
+  function pauseVideoseks() {
+    var videoseks = document.getElementById("tiktok6");
+    videoseks.pause();
+  }
+
+/*video7*/
+
+function playVideosyv() {
+    var videosyv = document.getElementById("tiktok7");
+    videosyv.play();
+  }
+  
+  function pauseVideosyv() {
+    var videosyv = document.getElementById("tiktok7");
+    videosyv.pause();
+  }
+
+/*video8*/
+
+function playVideootte() {
+    var videootte = document.getElementById("tiktok8");
+    videootte.play();
+  }
+  
+  function pauseVideootte() {
+    var videootte = document.getElementById("tiktok8");
+    videootte.pause();
+  }
